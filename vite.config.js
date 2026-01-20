07c9fad2a2ddaec2ed6bf36191d78a9ed23ce2c5
Michal <michal@MacBook-Air-Michal.local>
2026-01-20 09:50:50 +0100

    fix: update base path for custom domain root

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import { resolve } from 'path';
 
 export default defineConfig({
-    base: '/veloscope/',
+    base: '/',
     build: {
         rollupOptions: {
             input: {
